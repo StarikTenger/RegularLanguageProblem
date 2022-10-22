72e8f1aaf9c26774a19b01db7a9d4226e3f400fa
SoFa325 <sofyabel@inbox.ru>
2022-10-22 12:43:43 +0300

    Add Terminal class

---
+++ include/objects/terminal.h
@@ -0,0 +1,13 @@
+#ifndef TERMINAL_H
+#define TERMINAL_H
+
+class Terminal {
+
+  char m_name;
+
+public:
+  Terminal(char);
+  char name() const;
+};
+
+#endif // TERMINAL_H
---
+++ src/objects/terminal.cpp
@@ -0,0 +1,5 @@
+#include "terminal.h"
+
+Terminal::Terminal(char name) : m_name(name) {}
+
+char Terminal::name() const { return m_name; }
